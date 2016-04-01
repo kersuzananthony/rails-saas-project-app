2915a57f5329503936520d8c0ac4bf18f3dc43a8
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-01 21:03:04 +0800

    Adds email with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Mailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://kersuzan-saas-project.herokuapp.com/', :protocol => 'https' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
